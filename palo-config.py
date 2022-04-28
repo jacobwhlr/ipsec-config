fa227f910ab8ac35885e2d2a1a3680bc646ffc0b
jacobwhlr <jacobwhlr@gmail.com>
2022-04-28 10:03:33 -0400

    a

--- palo-config.py
+++ palo-config.py
@@ -4,10 +4,15 @@ proxy id = crypto acl
 Assumptions:
     Steve sucks
     Palo Public IP will be 70.70.70.70, interface will be Ethernet1/1
+    Zone: Name: VPN, Type: Layer3, Interfaces: tunnel.X
+    Virtual Router: default-vrf
+    Ike crypto profile: Name: ike-phase-1
+    IPSec crypto profile: Name: ipsec-phase-2
 
 Website:
     CLI: https://knowledgebase.paloaltonetworks.com/KCSArticleDetail?id=kA10g000000ClHsCAK
     GUI: https://knowledgebase.paloaltonetworks.com/KCSArticleDetail?id=kA10g000000ClJ3CAK
+         https://www.gns3network.com/ipsec-tunnel-between-palo-alto-and-cisco-asa-firewall/
          https://www.youtube.com/watch?v=Kmi4DjBFgwA
 
 """
@@ -31,14 +36,12 @@ def main():
 
     # Open a file
     # on_boarding_file = open("test_output.txt", "w")
-
-    # TODO: step X: create zone
-    # TODO: step X: create ike profile for phase 1
-    # TODO: step X: create ipsec profile for phase 2
-    # TODO: step X: create ike gateway
-
-
-
+    for i in range(0, 5):
+        print(f"set network interface tunnel units tunnel.{i + 1} ipv6 enabled no")
+        print(f"set network interface tunnel units tunnel.{i + 1} comment '{EXT_IP[i]} VPN'")
+        print(f"set zone vpn network layer3 tunnel.{i + 1}")
+        print(f"set network virtual-router 'default-vrf' interface [ tunnel.{i + 1} ]")
+        print("\n")
 
     # Close opend file
     # on_boarding_file.close()
